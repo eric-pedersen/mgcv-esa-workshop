f4764d65507f3d9536b5f2e644933b7c2833c361
David Lawrence Miller <github@ninepointeightone.net>
2016-08-03 23:12:23 -0400

    add script to correct mathjax location for html on site

---
+++ slides/correct_mathjax.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# correct the paths to the mathjax files for HTML versions of the slides
+
+# changes the link to be to a folder called mathjax-23 in the current directory
+# this avoids having lots of copes of the same mathjax stuff
+
+# also use decktape to make a PDF of the slides (probably only works on
+# Dave's laptop)
+
+for i in $( ls *.html); do
+  echo processing: $i
+  FN=${i/.html/}_files\\/
+  sed "s/$FN//" "$i" > tmp.html
+  mv tmp.html $i
+#  PDFFN=${i/.html/}.pdf
+#  ~/sources/decktape/bin/phantomjs ~/sources/decktape/decktape.js automatic -s 1024x768 $i $PDFFN
+done
