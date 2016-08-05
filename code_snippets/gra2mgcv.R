052c2601db29e2abcf9f87f1432bbd06d4e3ef9a
Gavin Simpson <ucfagls@gmail.com>
2016-08-04 23:03:22 -0600

    add a function to read and convert BayesX .gra files to nb list format required by mgcv's mrf smooth

---
+++ code_snippets/gra2mgcv.R
@@ -0,0 +1,19 @@
+## Read in a BayesX graph file and output list required by mgcv's mrf smooth
+gra2mgcv <- function(file) {
+    ## first row contains number of objects in file
+    ## graph info is stored in triplets or rows after first row
+    ##   - 1st row of triplet is ID
+    ##   - 2nd row of triplet is number of neighbours
+    ##   - 3rd row of triplet is vector of neighbour
+    gra <- readLines(file)
+    N <-  gra[1]
+    gra <- gra[-1]                      # pop the first row which contains N
+    ids <- seq.int(1, by = 3, length.out = N)
+    nbs <- seq.int(3, by = 3, length.out = N)
+    node2id <- function(nodes, ids) {
+        as.numeric(ids[as.numeric(nodes)])
+    }
+    l <- lapply(strsplit(gra[nbs], " "), node2id, ids = gra[ids])
+    names(l) <-  gra[ids]
+    l
+}
